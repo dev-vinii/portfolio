c12e84fc335137600f18f083df55ab3a7542be0f
vinicius <viniciusluna@yahoo.com>
2024-05-17 15:56:47 -0300

    feat: setting route

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SideBar from "./components/Sidebar/Sidebar.";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <>
+      <SideBar/>
+      <AppRoutes/>
+    </>
+  );
+}
+
+export default App;
