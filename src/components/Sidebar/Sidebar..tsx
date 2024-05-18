6b1b07e1fa72b47a9244abe29365dd5f3e02791a
vinicius <viniciusluna@yahoo.com>
2024-05-18 10:27:37 -0300

    feat: creating fuction to navigate

--- src/components/Sidebar/Sidebar..tsx
+++ src/components/Sidebar/Sidebar..tsx
@@ -2,17 +2,27 @@ import profile from "@/assets/profile-image.jpeg";
 import ProfileContent from "./ProfileContent/ProfileContent";
 import MenuContent from "./MenuContent/MenuContent";
 import { useEffect } from "react";
-import AppRoutes from "@/routes/AppRoutes";
+import routes from "@/routes/routes";
+// import { Navigate } from "react-router-dom";
 
 export default function SideBar() {
    useEffect(() => {
    }, []) 
     
+  //  function handleClickMenu(path: string) {
+  //    console.log(<Navigate to={`${path}`} />);
+  //    return <Navigate to={`${path}`} />;
+  //  }
+  
   return (
     <>
-      <div className="bg-sidebar min-h-screen absolute left-0 top-0 min-w-60 max-w-60">
+      <div className="bg-sidebar min-h-screen absolute left-0 top-0 min-w-72 max-w-60">
         <ProfileContent image={profile} name="Vinicius Luna" />
-        <MenuContent label="Profile"/>
+        {routes.map((route, i) => (
+          route.name && <MenuContent label={String(route.name)} key={i} onClick={function (): void {
+            throw new Error("Function not implemented.");
+          } } />
+        ))}
       </div>
     </>
   );
