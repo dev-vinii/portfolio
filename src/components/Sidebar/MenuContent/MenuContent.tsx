8f97be8b6be33db9b356f01df8f1a290c606d1f9
vinicius <viniciusluna@yahoo.com>
2024-05-18 10:27:46 -0300

    feat: adding prop

--- src/components/Sidebar/MenuContent/MenuContent.tsx
+++ src/components/Sidebar/MenuContent/MenuContent.tsx
@@ -1,6 +1,7 @@
-export default function MenuContent(props: { label: string }) {
+export default function MenuContent(props: { label: string, onClick: () => void }) {
   return (
-    <div className="h-10 cursor-pointer flex justify-center items-center w-full mt-16 rounded-lg bg-sidebar transition-all delay-100 hover:bg-sidebarColor">
+    <div className="h-10 cursor-pointer flex justify-center items-center w-full mt-16 rounded-lg bg-sidebar transition-all delay-100 hover:bg-sidebarColor"
+    onClick={props.onClick}>
       <span>{props.label}</span>
     </div>
   );
