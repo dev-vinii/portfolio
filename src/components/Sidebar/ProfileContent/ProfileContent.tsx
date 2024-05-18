835cd225d849bcd7d14a8306a31d1ebc493d7b09
vinicius <viniciusluna@yahoo.com>
2024-05-18 10:28:00 -0300

    feat: adding flex to sidebar image

--- src/components/Sidebar/ProfileContent/ProfileContent.tsx
+++ src/components/Sidebar/ProfileContent/ProfileContent.tsx
@@ -1,8 +1,8 @@
 export default function Profile(props: { image: string, name: string }) {
   return (
-    <div className="p-8">
-      <img src={props.image} className="rounded-full" />
-      <div className="font-mono flex justify-center mt-4">
+    <div className="p-8 flex flex-col items-center">
+      <img src={props.image} className="rounded-full h-40 w-40" />
+      <div className="font-mono mt-4">
         <span>{props.name}</span>
       </div>
     </div>
