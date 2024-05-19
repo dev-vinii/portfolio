5fbf9c260a5b3699a8ee600376ebf24bc540c338
vinicius <viniciusluna@yahoo.com>
2024-05-19 01:48:44 -0300

    feat: adding icon to sidebar

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import NotFound from "@/pages/NotFound/NotFound";
 import Portfolio from "@/pages/Portfolio/Portfolio";
+import { CgProfile } from 'react-icons/cg';
+
 
 const routes = [
-  { path: '/', component: Portfolio, name: 'Profile', },
+  { path: '/', component: Portfolio, name: 'Profile', sidebarIcon: CgProfile},
   { path: '*', component: NotFound}
 ];
 
