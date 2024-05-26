4a18d33f481e3cea591e70a5e8ef4b827c2cf1c2
Vinicius <viniciusluna@yahoo.com>
2024-05-26 15:17:04 -0300

    feat: download file

--- src/components/PdfButton/PdfButton.tsx
+++ src/components/PdfButton/PdfButton.tsx
@@ -1,9 +1,20 @@
 import { FaDownload } from "react-icons/fa6";
 
 export default function PdfButton() {
+  
+  const downloadCV = () => {
+  const resume = document.createElement("a");
+    const fileUrl = "src/assets/pdf/ViniciusLuna.pdf"
+    resume.href = fileUrl;
+    resume.download = "ViniciusLuna.pdf";
+    document.body.appendChild(resume);
+    resume.click();
+    document.body.removeChild(resume);
+  }
+  
   return (
     <div>
-      <button className="bg-sidebar border-none font-nunito flex gap-2 items-center 
+      <button onClick={downloadCV} className="bg-sidebar border-none font-nunito flex gap-2 items-center 
       justify-center hover:border-sidebar focus:outline-none transition-all duration-150">
         <FaDownload />
         Baixar CV
