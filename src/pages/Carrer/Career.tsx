1b3860c51e95d2705466f6d8effd2bfb3f5968c4
Vinicius <viniciusluna@yahoo.com>
2024-06-29 09:47:36 -0300

    feat: setting global css

--- src/pages/Carrer/Career.tsx
+++ src/pages/Carrer/Career.tsx
@@ -1,5 +1,9 @@
 export default function Career() {
   return (
-    <div>Teste</div>
+    <div className="flex w-full h-full items-center max-sm:flex-co justify-center">
+      <div className="h-2/3 w-4/5 border-l-2 border-black p-12">
+        <div></div>
+      </div>
+    </div>
   )
 }
